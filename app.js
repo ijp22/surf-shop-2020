f0c5fe87e07277d130f83fe6c114ffe835c6281f
itisianpetts <itisianpetts@gmail.com>
2021-01-04 16:26:20 +0000

    Add Post Routes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const createError = require('http-errors');
 const express = require('express');
+const favicon = require('serve-favicon');
 const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const index = require('./routes/index');
+const posts = require('./routes/posts');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -19,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/posts', posts);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
