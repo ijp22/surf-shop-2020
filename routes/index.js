f7b83db08cbd0f8e105c1945cf45bd4afeaeac2a
itisianpetts <itisianpetts@gmail.com>
2021-01-04 17:08:00 +0000

    Add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surg Shop - Home' });
+});
+
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('Get /register');
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('Get /profile');
+});
+
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/user_id');
+});
+
+/* GET /forgot-password */
+router.get('/forgot', (req, res, next) => {
+  res.send('Get /forgot');
+});
+
+/* PUT /forgot-password */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+/* GET /reset-password */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('Get /reset/:token');
+});
+
+/* PUT /reset-password */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset/:token');
 });
 
 module.exports = router;
