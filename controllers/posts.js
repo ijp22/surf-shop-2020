5152b58b905f78064143a767167625af22303ef5
itisianpetts <itisianpetts@gmail.com>
2021-01-08 11:12:24 +0000

    Add Image Upload - Create

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'dysu2ctc9',
+    api_key: '922687758451428',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
     // Post Index
@@ -14,6 +20,14 @@ module.exports = {
 
     // Create Post
     async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for(const file of req.files) {
+           let image = await cloudinary.v2.uploader.upload(file.path);
+           req.body.post.images.push({
+               url: image.secure_url,
+               public_id: image.public_id
+           });
+        }
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
