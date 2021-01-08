697adcace1e8cbf2400fb44ccce6fb83f90bcd9c
itisianpetts <itisianpetts@gmail.com>
2021-01-08 13:53:05 +0000

    Add Image Upload and Delete

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,11 +22,11 @@ module.exports = {
     async postCreate(req, res, next) {
         req.body.post.images = [];
         for(const file of req.files) {
-           let image = await cloudinary.v2.uploader.upload(file.path);
-           req.body.post.images.push({
-               url: image.secure_url,
-               public_id: image.public_id
-           });
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({
+                url: image.secure_url,
+                public_id: image.public_id
+            });
         }
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
@@ -45,13 +45,43 @@ module.exports = {
 
     // Update post
     async postUpdate(req, res, next) {
-        let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+        let post = await Post.findById(req.params.id);
+        if(req.body.deleteImages && req.body.deleteImages.length){
+            let deleteImages = req.body.deleteImages;
+            for(const public_id of deleteImages){
+                await cloudinary.v2.uploader.destroy(public_id);
+                for(const image of post.images) {
+                    if(image.public_id === public_id){
+                        let index = post.images.indexOf(image);
+                        post.images.splice(index, 1);
+                    }
+                }   
+            }
+        }
+        if(req.files) {
+            for(const file of req.files) {
+                let image = await cloudinary.v2.uploader.upload(file.path);
+                post.images.push({
+                    url: image.secure_url,
+                    public_id: image.public_id
+                });
+             }
+        }
+        post.title = req.body.post.title;
+        post.description = req.body.post.description;
+        post.price = req.body.post.price;
+        post.location = req.body.post.location;
+        post.save();
         res.redirect(`/posts/${post.id}`);
     },
 
     // Detroy post
     async postDestroy(req, res, next) {
         let post = await Post.findByIdAndRemove(req.params.id);
+        for(const image of post.images) {
+            await cloudinary.v2.uploader.destroy(image.public_id);
+        }
+        await post.remove();
         res.redirect('/posts');
     }
 }
