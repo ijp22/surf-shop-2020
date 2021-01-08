6b2beb5b9387c3654e8106bf19c02be22448ebe9
itisianpetts <itisianpetts@gmail.com>
2021-01-08 16:40:58 +0000

    Add Map and Mapbox

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
 //Connect to the database
-mongoose.connect('mongodb://localhost:27017/surf-shop', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/surf-shop-mapbox', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 const cloudinary = require('cloudinary');
 cloudinary.config({
     cloud_name: 'dysu2ctc9',
@@ -28,6 +30,13 @@ module.exports = {
                 public_id: image.public_id
             });
         }
+        let response = await geocodingClient
+            .forwardGeocode({
+                query: req.body.post.location,
+                limit: 1
+            })
+            .send();
+            req.body.post.coordinates = response.body.features[0].geometry.coordinates;
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },
@@ -58,6 +67,7 @@ module.exports = {
                 }   
             }
         }
+        // Check Images
         if(req.files) {
             for(const file of req.files) {
                 let image = await cloudinary.v2.uploader.upload(file.path);
@@ -67,10 +77,23 @@ module.exports = {
                 });
              }
         }
+        // Check if location was updated
+        if(req.body.post.location !== post.location){
+            let response = await geocodingClient
+                .forwardGeocode({
+                query: req.body.post.location,
+                limit: 1
+                })
+                .send();
+            post.coordinates = response.body.features[0].geometry.coordinates;
+            post.location = req.body.post.location;
+        }
+
+        // update properties
         post.title = req.body.post.title;
         post.description = req.body.post.description;
         post.price = req.body.post.price;
-        post.location = req.body.post.location;
+        
         post.save();
         res.redirect(`/posts/${post.id}`);
     },
