8edea4eb903ce4b84002f90f47bb5d105d2bc4a2
itisianpetts <itisianpetts@gmail.com>
2021-01-15 13:53:32 +0000

    Add Profile Image

--- models/user.js
+++ models/user.js
@@ -3,12 +3,11 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    image: String
+	email: { type: String, unique: true, required: true },
+	image:  {
+		secure_url: { type: String, default: '/images/default-profile.jpg' },
+		public_id: String
+	}
 });
 
 UserSchema.plugin(passportLocalMongoose);
